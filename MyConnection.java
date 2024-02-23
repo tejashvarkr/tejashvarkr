911c95d06a400f6dbb677986c147cfc7db49c283
tejashvarkr <155559129+tejashvarkr@users.noreply.github.com>
2024-02-23 18:08:22 +0530

    Add files via upload

---
+++ MyConnection.java
@@ -0,0 +1,13 @@
+
+package db;
+
+/**
+ *
+ * @author tejas
+ */
+public class MyConnection {
+   private static final String username="";
+   private static final String password = "";
+   private static final dataConn ="jdbc:mysql://localhost:3306/;
+   
+}
